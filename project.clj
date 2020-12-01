a3ca0062895b8be1e49381e45cbf395f90ce8e51
Nicolas Bertrand <bertrand.nicolas.90@gmail.com>
2020-12-01 13:07:05 +0200

    Initial commit

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject aoc2020 "0.1.0-SNAPSHOT"
+  :description "Advent of Code 2020"
+  :url "https://github.com/tampix/aoc2020"
+  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :dependencies [[org.clojure/clojure "1.10.1"]]
+  :repl-options {:init-ns clojure.core})
